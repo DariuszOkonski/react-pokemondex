5c1bc90110dca4b780349d2b2301d9052aa1b3a0
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 16:16:58 +0000

    display component PlayGame

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import Player from './Player/Player';
+import PlayGame from './PlayGame/PlayGame';
+
 
 class App extends Component {
   state = {
     isLoading: true,
   }
 
-  drawPokemons(list) {
-    let tempArr = [];
-    let listLength = list.length / 2;
-    for (let i = 0; i < listLength; i++) {
-      const index = Math.floor(Math.random() * list.length);
-      tempArr.push(list.splice(index, 1)[0]);
-    }
-    return tempArr;
-  }
-
-  getExperience(list) {
-    return list.reduce(function (acc, obj) {
-      return acc + obj.experience
-    }, 0);
-  }
-
   render() {
-    const pokemons = [...this.props.pokemons];
-    const hand1 = this.drawPokemons(pokemons);
-    const hand2 = [...pokemons];
-
     return (
-      <div className="App">
-        <Player list={hand1} name="Human" exp={this.getExperience(hand1)} />
-        <Player list={hand2} name="Cpu" exp={this.getExperience(hand2)} />
-      </div>
-    );
+      <PlayGame pokemons={this.props.pokemons} />
+    )
   }
 }
 
