2e62f426b22fc0b7acc89a77bab6a7824be15ee1
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 16:02:43 +0000

    add props pokemons

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import pokemons from './data/pokemons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App pokemons={pokemons} />, document.getElementById('root'));
