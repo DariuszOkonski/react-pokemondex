688bd68fd2bed72c316529d5b1628cd2fdab3c71
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 13:47:07 +0000

    Updates

---
+++ precache-manifest.fb6fc6a93a2e2af1b7bd2ba7cb6480eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da535bbb6960c2fd1932033ce6ef96f7",
+    "url": "/react-pokemondex/index.html"
+  },
+  {
+    "revision": "d3445283b14c2f1c31ef",
+    "url": "/react-pokemondex/static/css/main.f73eff45.chunk.css"
+  },
+  {
+    "revision": "c4cef838332e5f174852",
+    "url": "/react-pokemondex/static/js/2.a5ab904e.chunk.js"
+  },
+  {
+    "revision": "d3445283b14c2f1c31ef",
+    "url": "/react-pokemondex/static/js/main.937cbbea.chunk.js"
+  },
+  {
+    "revision": "679cb366167bc7dfe01b",
+    "url": "/react-pokemondex/static/js/runtime-main.697ce22c.js"
+  }
+]);
