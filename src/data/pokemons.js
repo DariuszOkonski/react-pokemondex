14eed1d7f8c33bb371561c4b3e4e1bfca4d08a07
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 14:27:31 +0000

    change id from single digit to triple digit

--- src/data/pokemons.js
+++ src/data/pokemons.js
@@ -1,73 +1,75 @@
-export const pokemons = [{
-    id: 1,
+const pokemons = [{
+    id: 111,
     name: 'Squirtle',
     type: 'water',
     experience: 63
   },
   {
-    id: 2,
+    id: 114,
     name: 'Charmander',
     type: 'fire',
     experience: 62
   },
   {
-    id: 3,
+    id: 132,
     name: 'Butterfree',
     type: 'flying',
     experience: 178
   },
   {
-    id: 4,
+    id: 143,
     name: 'Gengar',
     type: 'poison',
     experience: 225
   },
   {
-    id: 5,
+    id: 151,
     name: 'Meatpod',
     type: 'bug',
     experience: 112
   },
   {
-    id: 6,
+    id: 168,
     name: 'Pikachu',
     type: 'electric',
     experience: 72
   },
   {
-    id: 7,
+    id: 172,
     name: 'Jigglypuff',
     type: 'normal',
     experience: 95
   },
   {
-    id: 8,
+    id: 183,
     name: 'Eevee',
     type: 'water',
     experience: 65
   },
   {
-    id: 9,
+    id: 199,
     name: 'Swordfish',
     type: 'bug',
     experience: 210
   },
   {
-    id: 10,
+    id: 210,
     name: 'Turtlefee',
     type: 'flying',
     experience: 83
   },
   {
-    id: 11,
+    id: 211,
     name: 'Rubbitpuff',
     type: 'poison',
     experience: 176
   },
   {
-    id: 12,
+    id: 212,
     name: 'Gengareye',
     type: 'water',
     experience: 162
   },
-]
+];
+
+export default pokemons;
