e7cac69c9653a50223d50463b957543be3cea2df
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 13:57:37 +0000

    add pokemons data at file pokemons.js

---
+++ src/data/pokemons.js
@@ -0,0 +1,73 @@
+export const pokemons = [{
+    id: 1,
+    name: 'Squirtle',
+    type: 'water',
+    experience: 63
+  },
+  {
+    id: 2,
+    name: 'Charmander',
+    type: 'fire',
+    experience: 62
+  },
+  {
+    id: 3,
+    name: 'Butterfree',
+    type: 'flying',
+    experience: 178
+  },
+  {
+    id: 4,
+    name: 'Gengar',
+    type: 'poison',
+    experience: 225
+  },
+  {
+    id: 5,
+    name: 'Meatpod',
+    type: 'bug',
+    experience: 112
+  },
+  {
+    id: 6,
+    name: 'Pikachu',
+    type: 'electric',
+    experience: 72
+  },
+  {
+    id: 7,
+    name: 'Jigglypuff',
+    type: 'normal',
+    experience: 95
+  },
+  {
+    id: 8,
+    name: 'Eevee',
+    type: 'water',
+    experience: 65
+  },
+  {
+    id: 9,
+    name: 'Swordfish',
+    type: 'bug',
+    experience: 210
+  },
+  {
+    id: 10,
+    name: 'Turtlefee',
+    type: 'flying',
+    experience: 83
+  },
+  {
+    id: 11,
+    name: 'Rubbitpuff',
+    type: 'poison',
+    experience: 176
+  },
+  {
+    id: 12,
+    name: 'Gengareye',
+    type: 'water',
+    experience: 162
+  },
+]
