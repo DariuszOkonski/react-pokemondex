4f693815a500d0888928c5f543cab229dc736411
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 15:33:43 +0000

    add all features to Player component

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import CardsList from '../CardsList/CardsList';
-
+import styles from './Player.module.css';
 import PropTypes from 'prop-types';
 
-const Player = ({ list }) => {
+const Player = ({ list, name, exp }) => {
   return (
-    <div>
-      <h2>Player</h2>
-
+    <div className={styles.container}>
+      <h2 className={styles.header}>{name}</h2>
+      <p className={styles.paragraph}>Total Experience: {exp}</p>
       <CardsList list={list} />
     </div>
   );
 }
 
 Player.propTypes = {
-  list: PropTypes.array.isRequired
+  list: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
+  exp: PropTypes.number.isRequired
 }
 
 export default Player;
