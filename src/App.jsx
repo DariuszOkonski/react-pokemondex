c6c2ebea91cb177bc45781c6a714f24b781f694b
Dariusz Okonski <darek200180@gmail.com>
2019-11-16 13:42:11 +0000

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// API do charakterów pokemonów muszę znaleźć albo stworzyć taką tablicę
